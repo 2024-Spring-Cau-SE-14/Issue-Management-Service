fd0ca2b7421131a6ea1785bf97dfdaf9f2efabdf
papercd <hpptknk10@hotmail.co.kr>
2024-05-29 17:22:34 +0900

    database code finished, remote database and testing needed.

--- application/src/main/java/com/se14/repository/db_impl/Main.java
+++ application/src/main/java/com/se14/repository/db_impl/Main.java
@@ -1,11 +1,6 @@
 package com.se14.repository.db_impl;
 
 import com.se14.domain.Issue;
-import com.se14.domain.User;
-import com.se14.repository.IssueRepository;
-import com.se14.repository.db_impl.DBInitializer;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Optional;
 
 public class Main {
